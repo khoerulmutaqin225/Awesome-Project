db248c88bb506b174b96f49322576f7c9c4c63f6
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-08-11 10:09:55 +0800

    CARA MEMBUAT PAGE LOADER MENGGUNAKAN SKELETON

--- src/screens/News.js
+++ src/screens/News.js
@@ -5,7 +5,19 @@ import React, { useState, useEffect } from 'react'
 import { WARNA_ABU_ABU, WARNA_WARNING } from '../Awesome-Project/src/utils/constant'
 import Title from './Title'
 import axios from 'axios';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+
 // BATAS IMPORT
+
+
+// {data ? (
+//   data.map((item, i) => { #your code
+//     ): (
+//       <><Text>Loading....</Text></>
+//        )
+//   }
+
+// Cara membacanya, Jika ada data lakukan mappping pada program, jika belum ada tampilkan tulisan teks
 
 // BATAS FUNCTION
 const Home = ({ navigation }) => {
@@ -28,8 +40,11 @@ const Home = ({ navigation }) => {
   };
 
   useEffect(() => {
-    getData()
+    setTimeout(()=>{
+    getData();
+    }, 3000);
   }, [])
+  // 3000 artinya 3 second
   // BATAS FUNCTION
 
 
@@ -40,35 +55,103 @@ const Home = ({ navigation }) => {
       <SafeAreaView style={{ flex: 1 }}>
         <Title />
         <ScrollView style={{ flex: 1 }}>
-          {data &&
-            data.map((item, i) => {
-              return (
-                <View>
-                  <TouchableOpacity
-                    style={{ flex: 1 }}
-                    // data: item (berfungsi agar data yang dikirim berdasarkan id)
-                    onPress={() => navigation.navigate('Home', { data: item })}>
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        marginVertical: 10,
-                        marginHorizontal: 5,
-                        backgroundColor: 'white',
-                        flex: 1
-                      }}>
-                      <Image
-                        style={{ width: 75, height: 75 }}
-                        source={{ uri: item.urlToImage }}
-                      />
-                      <View style={{ justifyContent: 'space-between' }}>
-                        <Text style={{ alignSelf: 'center', fontSize: 10, fontWeight: 'bold' }}>{item.title}</Text>
-                        <Text style={{ fontSize: 10 }}>{item.author}</Text>
+          <View>
+            {data ? (
+              data.map((item, i) => {
+                return (
+                  <View key={i}>
+                    <TouchableOpacity
+
+                      style={{ flex: 1 }}
+                      // data: item (berfungsi agar data yang dikirim berdasarkan id)
+                      onPress={() => navigation.navigate('Home', { data: item })}>
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          marginVertical: 10,
+                          marginHorizontal: 5,
+                          backgroundColor: 'white',
+                          flex: 1
+                        }}>
+                        <Image
+                          style={{ borderRadius: 10, width: 75, height: 75 }}
+                          source={{ uri: item.urlToImage }}
+                        />
+                        <View style={{ justifyContent: 'space-between' }}>
+                          <Text style={{ alignSelf: 'center', fontSize: 10, fontWeight: 'bold' }}>{item.title}</Text>
+                          <Text style={{ fontSize: 10 }}>{item.author}</Text>
+                        </View>
                       </View>
-                    </View>
-                  </TouchableOpacity>
+                    </TouchableOpacity>
+                  </View>
+                );
+              })
+            ) : (
+              // <><Text>Loading....</Text></>
+              <SkeletonPlaceholder >
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', margin: 10 }}>
+
+                  <View style={{ width: 100, height: 100, borderRadius: 10 }} />
+
+                  <View style={{ marginLeft: 20 }}>
+                    <View style={{ width: 120, height: 20, borderRadius: 4 }} />
+                    <View
+                      style={{ marginTop: 6, width: 80, height: 20, borderRadius: 4 }} />
+                  </View>
                 </View>
-              );
-            })}
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', margin: 10 }}>
+
+                  <View style={{ width: 100, height: 100, borderRadius: 10 }} />
+
+                  <View style={{ marginLeft: 20 }}>
+                    <View style={{ width: 120, height: 20, borderRadius: 4 }} />
+                    <View
+                      style={{ marginTop: 6, width: 80, height: 20, borderRadius: 4 }} />
+                  </View>
+                </View>
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', margin: 10 }}>
+
+                  <View style={{ width: 100, height: 100, borderRadius: 10 }} />
+
+                  <View style={{ marginLeft: 20 }}>
+                    <View style={{ width: 120, height: 20, borderRadius: 4 }} />
+                    <View
+                      style={{ marginTop: 6, width: 80, height: 20, borderRadius: 4 }} />
+                  </View>
+                </View>
+
+              </SkeletonPlaceholder>
+              // <><Text>Loading....</Text></>
+              //   <SkeletonPlaceholder borderRadius={4}>
+              //   <SkeletonPlaceholder.Item 
+              //     flexDirection="row" 
+              //     alignItems="center"
+              //     >
+              //     <SkeletonPlaceholder.Item 
+              //       width={60}
+              //       height={60} 
+              //       borderRadius={50}
+              //     />
+              //     <SkeletonPlaceholder.Item marginLeft={20}>
+              //       <SkeletonPlaceholder.Item 
+              //         width={120} 
+              //         height={20} 
+              //       />
+              //       <SkeletonPlaceholder.Item 
+              //         marginTop={6} 
+              //         width={80} 
+              //         height={20} 
+              //       />
+              //     </SkeletonPlaceholder.Item>
+              //   </SkeletonPlaceholder.Item>
+              // </SkeletonPlaceholder>
+            )
+            }
+
+          </View>
         </ScrollView>
       </SafeAreaView>
     </>
